9fa4b29611bb8f4897ac06ee5299fc0eb21ac931
zylotech-tech <zylo.tech.ai@gmail.com>
2026-07-13 16:40:26 -0500

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Projects from './components/Projects'
+import WhyChooseUs from './components/WhyChooseUs'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-ink overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Projects />
+        <WhyChooseUs />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
